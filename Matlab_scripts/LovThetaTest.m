9065baa7480af2985d0b30005bb46b51f992ff89
Colton Griffin <67336510+chemfinal-dot@users.noreply.github.com>
2021-07-07 15:30:03 -0400

    Add files via upload

---
+++ Matlab_scripts/LovThetaTest.m
@@ -0,0 +1,43 @@
+function [Theta, X1] = LovThetaTest(G)
+%LovTheta Computes first formulation of Lovasz Theta
+%   Input - adjacency matrix for a graph G, size n x n
+
+[numRows, n] = size(G);
+
+% Construct and solve the model
+J = ones(n);
+P = eye(n)+G;
+cvx_begin sdp quiet
+    variable M(n^2+n+1,n^2+n+1) semidefinite;
+    variable t
+    minimize( t );
+    M(1:n, n+1:n^2+n+1) == zeros(n, n^2+1);
+    M(1+n:n^2+n, 1+n:n^2+n) == eye(n^2);
+    for i = 1:n
+        for j = 1:n
+            M(i,i) == 1;
+            if (G(i,j) == 0) && (i ~= j)
+                M(i,j) == 0;
+            end
+        end
+    end
+    for i = 0:(n^2-1)
+        q = floor(i/n);
+        r = mod(i,n);
+        M(n+1+i,n^2+n+1) == M(r+1,q+1);
+    end
+    M(n^2+n+1,n^2+n+1) - t == 2*trace(P*M(1:n,1:n))-trace(P*P); 
+    
+cvx_end
+
+Theta = t;
+%Theta = (trace((A-G)*(A-G))/n^2)^(-1);
+%for i=1:n
+%    for j=1:n
+%        Theta = Theta + (A(i,j)-G(i,j))^2/n^2;
+%    end
+%end
+%Theta = Theta^(-1)*3/4;
+X1 = M(1:n,1:n);
+end
+
