d9c6c6117c20a071387ed4f916af046124bbb203
Colton Griffin <67336510+chemfinal-dot@users.noreply.github.com>
2021-10-05 17:59:38 -0400

    Delete Matlab_scripts directory

--- Matlab_scripts/graphs/cart_product.m
+++
@@ -1,9 +0,0 @@
-function M = cart_product(G1,G2)
-%CART_PRODUCT Summary of this function goes here
-%   Detailed explanation goes here
-[numRows, n1] = size(G1);
-[numRows, n2] = size(G2);
-
-M = kron(G1,eye(n2)) + kron(eye(n1), G2);
-end
-
--- Matlab_scripts/graphs/lattice.m
+++
@@ -1,9 +0,0 @@
-function [M,m,alpha] = lattice(n)
-% Returns lattice graph (i.e. 3D square lattice graph)
-% Independence number = ceil(n^3/2)
-[path1,N1,a1] = path(n1);
-M = cart_product(cart_product(path1,path1),path1);
-[numRows, m] = size(M);
-alpha = ceil(n^3/2);
-end
-
